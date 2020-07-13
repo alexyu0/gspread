ecfb58482d04adc9098778d64ad9e67989601000
Alex Yu <alex.yu@affirm.com>
2020-07-12 23:24:03 -0700

    rate limit check in extensions, color conditional formatting

--- gspread/extensions/extensions.py
+++ gspread/extensions/extensions.py
@@ -4,6 +4,7 @@ import json
 import os
 import logging
 from datetime import datetime
+import random
 
 
 import requests
@@ -41,7 +42,7 @@ class GSpread:
         webbrowser.open('{}:{}/authorize'.format(self.host, self.port))
         stale_token = self.access_token
         while True:
-            print("in loop", stale_token, self.access_token)
+            print('in loop', stale_token, self.access_token)
             if os.path.exists(self.access_token_path):
                 with open(self.access_token_path, 'r') as f:
                     self.access_token = json.load(f)
@@ -61,6 +62,38 @@ class GSpread:
                 if pass_fn and pass_fn(result):
                     print('pass_fn {} passed, breaking loop'.format(pass_fn))
                     break
+    
+    def __request_loop(self, body):
+        max_backoff=32
+        i = 0
+        while True:
+            resp = requests.post(self.url_template.format('batchUpdate'), 
+                headers=self.auth_header,
+                json=body)
+            if resp.status_code == 429:
+                # rate limited by the googs
+                rand_ms = float(random.uniform(0, 1000))
+                sleep_time = min(2**i + rand_ms/1000, max_backoff)
+                log.info('Rate limited (error code 429), sleeping for {}'.format(sleep_time))
+                sleep(sleep_time)
+                i += 1
+                continue
+            elif not resp.ok:
+                if resp.status_code == 401:
+                    # auth error so refresh token then break since auth loop will do the work
+                    self.__auth_loop(
+                        requests.post, 
+                        [self.url_template.format('batchUpdate')],
+                        {
+                            'headers': self.auth_header,
+                            'json': body,
+                        },
+                        lambda resp: resp.ok,
+                    )
+                else:
+                    raise Exception(resp.json())
+
+            break
 
     def create_sheet_at_index(self, title, index, rows, cols):
         body = {
@@ -80,54 +113,93 @@ class GSpread:
                 },
             ],
         }
-        while True:
-            resp = requests.post(self.url_template.format('batchUpdate'), 
-                headers=self.auth_header,
-                json=body)
-            if not resp.ok:
-                log.error(resp.json())
-                if resp.status_code == 401:
-                    # auth error so refresh token then break since auth loop will do the work
-                    self.__auth_loop(
-                        requests.post, 
-                        [self.url_template.format('batchUpdate')],
-                        {
-                            'headers': self.auth_header,
-                            'json': body,
-                        },
-                        lambda resp: resp.ok,
-                    )
+        self.__request_loop(body)
 
-            break
-
-    def duplicate_sheet(self, id, title, index):
+    def duplicate_sheet(self, sheet_id, title, index):
         body = {
             'requests': [
                 {
                     'duplicateSheet': {
-                        'sourceSheetId': id,
+                        'sourceSheetId': sheet_id,
                         'newSheetName': '{}_copy_{}'.format(title, datetime.now()),
                         'insertSheetIndex': index + 1,
                     },
                 },
             ],
         }
-        while True:
-            resp = requests.post(self.url_template.format('batchUpdate'), 
-                headers=self.auth_header,
-                json=body)
-            if not resp.ok:
-                log.error(resp.json())
-                if resp.status_code == 401:
-                    # auth error so refresh token then break since auth loop will do the work
-                    self.__auth_loop(
-                        requests.post, 
-                        [self.url_template.format('batchUpdate')],
-                        {
-                            'headers': self.auth_header,
-                            'json': body,
-                        },
-                        lambda resp: resp.ok,
-                    )
+        self.__request_loop(body)
 
-            break
+    def clear_conditional_formatting(self, sheet_id):
+        while True:
+            try:
+                self.__request_loop({
+                    'requests': [
+                        {
+                            'deleteConditionalFormatRule': {
+                                'index': 0,
+                                'sheetId': sheet_id,
+                            },
+                        },
+                    ],
+                })
+            except Exception as e:
+                log.info('clearing done')
+                break
+
+    def color_conditional_format(self, 
+                                 sheet_id, 
+                                 start_col,
+                                 start_row,
+                                 end_col,
+                                 end_row,
+                                 threshold=None,
+                                 rgb_tup=(255,255,255),
+                                 lte=False,
+                                 gte=False):
+        # bool condition type
+        cond_type = None
+        if lte:
+            cond_type = 'NUMBER_LESS_THAN_EQ'
+        elif gte:
+            cond_type = 'NUMBER_GREATER_THAN_EQ'
+        else:
+            raise 'Invalid condition, choose either lte or gte'
+
+        body = {
+            'requests': [
+                {
+                    'addConditionalFormatRule': {
+                        'rule': {
+                            'ranges': [
+                                {
+                                    'sheetId': sheet_id,
+                                    'startColumnIndex': start_col,
+                                    'startRowIndex': start_row,
+                                    'endColumnIndex': end_col,
+                                    'endRowIndex': end_row,
+                                },
+                            ],
+                            'booleanRule': {
+                                'condition': {
+                                    'type': cond_type,
+                                    'values': [
+                                        {
+                                            'userEnteredValue': threshold,
+                                        }
+                                    ]
+                                },
+                                'format': {
+                                    'backgroundColor': {
+                                        'red': rgb_tup[0]/255.0,
+                                        'green': rgb_tup[1]/255.0,
+                                        'blue': rgb_tup[2]/255.0,
+                                    }
+                                }
+                            }
+                        },
+                        'index': 0
+                    }
+                }
+            ]
+        }
+        self.__request_loop(body)
